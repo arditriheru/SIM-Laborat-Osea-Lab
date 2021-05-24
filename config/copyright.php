2c14f66a1c3c4e85951fa7ec55ba18ec1cb07403
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-05-24 14:19:11 +0700

    Add files via upload

--- config/copyright.php
+++ config/copyright.php
@@ -7,6 +7,5 @@
 	Email       : arditriheruh@gmail.com
 	Website     : https://www.arditriheru.com
 -->
-<div align="center"><b>Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script></b> | RSKIA Permata.  
-	<br><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <!-- <a href="https://instagram.com/arditriheru"><b class="bluetext">RSKIA Permata</b></a> <br><font face="consolas" >Version 1.0</font> --></div>
+<div align="center">Copyright &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script><a expr:href='data:blog.homepageUrl'><data:blog.title/></a> <a href="https://instagram.com/arditriheru"> | Ardi Tri Heru</a></div></div>
 <!-- 	<br><br> -->
